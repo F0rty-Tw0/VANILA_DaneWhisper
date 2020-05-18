c20aeaf93a46d057f66e1fd10cfdb4d1bc431715
Artiom Tofan <artiomtofancv@gmail.com>
2020-05-18 17:26:27 +0200

    v0.3 - Added typed script to the header

--- JS/script.js
+++ JS/script.js
@@ -4,4 +4,11 @@ $(document).ready(() => {
 		play: 4000,
 		pagination: false
 	});
+
+	var typed = new Typed('.typed', {
+		strings: [ 'The ^100own^150er of "North^50ern Synth" ^200label.', 'Synth^100wave Pro^50du^100cer.' ],
+		typeSpeed: 50,
+		smartBackspace: true,
+		backSpeed: 30
+	});
 });
