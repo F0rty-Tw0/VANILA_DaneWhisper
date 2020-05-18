c63292e4271d793adc739221592c1d2936628144
Artiom Tofan <artiomtofancv@gmail.com>
2020-05-18 20:54:23 +0200

    v0.4 - Added about me card, made the header responsive

--- JS/script.js
+++ JS/script.js
@@ -6,7 +6,7 @@ $(document).ready(() => {
 	});
 
 	var typed = new Typed('.typed', {
-		strings: [ 'The ^100own^150er of "North^50ern Synth" ^200label.', 'Synth^100wave Pro^50du^100cer.' ],
+		strings: [ 'The ^100own^150er of "North^50ern Synth" ^200label^1000', 'Synth^100wave Pro^50du^100cer^5000 ' ],
 		typeSpeed: 60,
 		smartBackspace: true,
 		backSpeed: 30,
