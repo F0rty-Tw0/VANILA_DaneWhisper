ec73efb22527ad4ce86034a8ff2745d0e75bfd07
Artiom Tofan <artiomtofancv@gmail.com>
2020-05-19 12:33:35 +0200

    Added a carousel with achievements, added more animations, and loading intro

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,7 @@
 $(document).ready(() => {
+	$('.loading').fadeOut(1200);
+	$('#loading').delay(1000).fadeOut(1000);
+
 	$('#slides').superslides({
 		animation: 'fade',
 		play: 4000,
@@ -11,7 +14,58 @@ $(document).ready(() => {
 		smartBackspace: true,
 		backSpeed: 30,
 		loop: true,
-		startDelay: 1000,
+		startDelay: 2500,
 		showCursor: false
+	});
+
+	$('.owl-carousel').owlCarousel({
+		loop: true,
+		items: 4,
+		responsive: {
+			0: {
+				items: 1
+			},
+			576: {
+				items: 2
+			},
+			768: {
+				items: 3
+			},
+			992: {
+				items: 4
+			}
+		}
+	});
+
+	var infoTopOffset = $('.info-section').offset().top;
+
+	$(window).scroll(function() {
+		if (window.pageYOffset > infoTopOffset - $(window).height() + 300) {
+			$('.chart').easyPieChart({
+				easing: 'easeInOut',
+				barColor: '#fff',
+				trackColor: false,
+				scaleColor: false,
+				lineWidth: 4,
+				size: 200,
+				onStep: function(from, to, percent) {
+					$(this.el).find('.percent').text(Math.round(percent * 23));
+				}
+			});
+		}
+	});
+
+	$('#section-card ').fadeOut(100);
+	var infoTopOffset = $('#about').offset().top;
+
+	$(window).scroll(function() {
+		if (window.pageYOffset > infoTopOffset - $(window).height()) {
+			$('#slides').css('z-index', 0);
+			$('#section-card ').fadeIn(200);
+		} else {
+			$('#section-card ').fadeOut(200, function() {
+				$('#slides').css('z-index', '0');
+			});
+		}
 	});
 });
