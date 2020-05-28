1825580432a6ed0212b427fa2ad2aa638fab111d
Artiom Tofan <artiomtofancv@gmail.com>
2020-05-28 18:16:44 +0200

    v1.8 testing minified version speed

--- JS/iframes2.js
+++ JS/iframes2.js
@@ -1,17 +1,1 @@
-$('.universe').attr(
-	'src',
-	'https://bandcamp.com/EmbeddedPlayer/album=162335030/size=small/bgcol=ffffff/linkcol=63b2cc/artwork=none/track=1936175622'
-);
-$('.stranger').attr(
-	'src',
-	'https://bandcamp.com/EmbeddedPlayer/album=162335030/size=small/bgcol=ffffff/linkcol=63b2cc/artwork=none/track=2179282302'
-);
-$('.discouragement').attr(
-	'src',
-	'https://bandcamp.com/EmbeddedPlayer/album=162335030/size=small/bgcol=ffffff/linkcol=63b2cc/artwork=none/track=2245360117'
-);
-$('.space').attr(
-	'src',
-	'https://bandcamp.com/EmbeddedPlayer/album=162335030/size=small/bgcol=ffffff/linkcol=63b2cc/artwork=none/track=3834611213'
-);
-console.log('frames 2')
+$(".universe").attr("src","https://bandcamp.com/EmbeddedPlayer/album=162335030/size=small/bgcol=ffffff/linkcol=63b2cc/artwork=none/track=1936175622"),$(".stranger").attr("src","https://bandcamp.com/EmbeddedPlayer/album=162335030/size=small/bgcol=ffffff/linkcol=63b2cc/artwork=none/track=2179282302"),$(".discouragement").attr("src","https://bandcamp.com/EmbeddedPlayer/album=162335030/size=small/bgcol=ffffff/linkcol=63b2cc/artwork=none/track=2245360117"),$(".space").attr("src","https://bandcamp.com/EmbeddedPlayer/album=162335030/size=small/bgcol=ffffff/linkcol=63b2cc/artwork=none/track=3834611213");
